897b307fbb4d38537cd8fa9cb0a87210832d8a30
Andrew Circelli <andrewcircelli@gmail.com>
2022-05-18 11:28:36 -0400

    set up getPreGameOddsByDate call and GET event listener

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const axios = require("axios");
-
-const getSportsData = () => {
+const apiKey = process.env.SPORTSDATAKEY;
+const getPreGameOddsByDateURL = (date, apiKey) => {
+  `https://api.sportsdata.io/v3/mlb/odds/json/GameOddsByDate/${date}?key=${apiKey}`;
+};
+const getPreGameOddsByDate = () => {
   axios
-    .get(myURL)
+    .get(getPreGameOddsByDateURL("2022-05-18", apiKey))
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
+
+// Event Listeners
+document.getElementById("get").addEventListener("click", getPreGameOddsByDate);
